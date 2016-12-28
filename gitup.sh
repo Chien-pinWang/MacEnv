4f978c3e19182575356f84d79bee5a6959adceec
Chien-pin Wang <Wang.ChienPin@gmail.com>
2016-12-28 15:11:26 +0800

    12/28/2016 15:11:26> Maintenance sync to git remote

--- gitup.sh
+++ gitup.sh
@@ -2,13 +2,13 @@
 
 REPO='/Users/cpw/.env'
 COMMIT_TIMESTAMP=`date '+%m/%d/%Y %H:%M:%S'`
-ORIGIN='https:Chien-pinWang@kaohGithub4512//github.com/Chien-pinWang/MacEnv.git'
+ORIGIN='https://Chien-pinWang:kaohGithub4512@github.com/Chien-pinWang/MacEnv.git'
 
 GIT=`which git`
 
 cd ${REPO}
 ${GIT} add .
-${GIT} commit -m "$dt> Maintenance sync to git remote"
+${GIT} commit -m "${COMMIT_TIMESTAMP}> Maintenance sync to git remote"
 ${GIT} checkout master
 ${GIT} merge develop
 ${GIT} push ${ORIGIN} master
