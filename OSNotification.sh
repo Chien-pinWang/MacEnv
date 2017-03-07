5cf4b7fb4400abc15bd600a0fca60202e3be28d1
Chien-pin Wang <Wang.ChienPin@gmail.com>
2017-03-07 11:12:07 +0800

    Added jrnl settings and OS X notification script

---
+++ OSNotification.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+MESSAGE="Message"
+TITLE="Title"
+SUBTITLE="Sub-Title"
+
+# osascript -e 'display notification "<message>" with title "<title>" subtitle "<sub-title>"'
+osascript -e 'display notification "3/17 打掃取消; 3/31 協助清掃四樓; 4 月起換約只打掃二樓" with title "心路打掃工作調整" subtitle "劉老師 0914-089-599"'
